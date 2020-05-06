44c037ee4979c9b7c67bab93861e946810a1790f
matheusbaars <matbav@gmail.com>
2020-05-06 09:58:19 -0300

    media inserida e modificação do model cliente com inclusao de horário e fatura

--- clientes/models.py
+++ clientes/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import datetime
 
 class Cliente(models.Model):
     nome_cliente = models.CharField(max_length=200)
@@ -7,6 +8,8 @@ class Cliente(models.Model):
     cep_cliente = models.CharField(max_length=9)
     telefone = models.CharField(max_length=11)
     email = models.CharField(max_length=200)
+    hora_registro = models.DateTimeField(default=datetime.now, blank=True, null=True)
+    fatura = models.ImageField(upload_to='faturas_clientes', blank=True, null=True, default='')
 
     def __str__(self):
         return self.nome_cliente
