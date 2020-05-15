224f7041bb204aa138b34b1cda49cbe5bb42c985
matheusbaars <matbav@gmail.com>
2020-05-14 21:48:17 -0300

    Configurando o Heroku

--- gerenciamento_solar/settings.py
+++ gerenciamento_solar/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from decouple import config
+from dj_database_url import parse as dburl
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,12 +22,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ')kbu&11o+g#c2)+n78dt0$8e((@lw+%5zt16herkps#!hx5^=e'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['gerenciamento-solar.herokuapp.com']
 
 
 # Application definition
@@ -78,6 +80,11 @@ WSGI_APPLICATION = 'gerenciamento_solar.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+
+default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
+
+DATABASES = {'default': config('DATABASE_URL', default=default_dburl, cast=dburl), }
+
 
 DATABASES = {
     'default': {
