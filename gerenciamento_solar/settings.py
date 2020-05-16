71fde4f3013d823901b83ede8d8169391d5f5795
matheusbaars <matbav@gmail.com>
2020-05-16 14:30:00 -0300

    Consertando BUG server

--- gerenciamento_solar/settings.py
+++ gerenciamento_solar/settings.py
@@ -88,19 +88,6 @@ DATABASES = {
     }
 
 
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'gerenciamento',
-        'USER': 'postgres',
-        'PASSWORD': 'admin',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
