32525683e6e2252ea3c7a48b59f2046d0fd9d61d
matheusbaars <matbav@gmail.com>
2020-05-16 19:27:43 -0300

    Correção das templates

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render, redirect
 from .forms import LoginForm
 from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def logado(request):
     return render(request, 'logado.html')
 
+@login_required
 def my_logout(request):
     logout(request)
     return render(request, 'index.html')
 
+@login_required
 def database(request):
     return render(request, 'database.html')
