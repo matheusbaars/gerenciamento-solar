9ec9cee1af14de7264fd7783249c9a2e3c46f09c
matheusbaars <matbav@gmail.com>
2020-05-16 13:47:28 -0300

    Consertando BUG server

--- gerenciamento_solar/settings.py
+++ gerenciamento_solar/settings.py
@@ -83,9 +83,9 @@ WSGI_APPLICATION = 'gerenciamento_solar.wsgi.application'
 
 default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
 
-# DATABASES = {
-#     'default': config('DATABASE_URL', default=default_dburl, cast=dburl), 
-#     }
+DATABASES = {
+    'default': config('DATABASE_URL', default=default_dburl, cast=dburl), 
+    }
 
 
 
